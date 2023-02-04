a675677be5f209e5c831d88d1f8a723b3cb05b35
briesenberg07 <ries07@uw.edu>
2023-02-03 16:59:18 -0800

    add comment on filepaths

--- Python2Saxon/test_py2xsl.py
+++ Python2Saxon/test_py2xsl.py
@@ -1,3 +1,4 @@
 import os
-# filepaths below are evaluated from *directory where the terminal is open*
+# filepaths in the os.system command below are evaluated from *directory where the terminal is open*
+# filepaths in the .xsl file are evaluated from that file's location
 os.system(f"java -cp ~/ries/saxon/saxon-he-10.3.jar net.sf.saxon.Transform -s:xsl/test_py2xsl.xsl -xsl:xsl/test_py2xsl.xsl word=theBird")
