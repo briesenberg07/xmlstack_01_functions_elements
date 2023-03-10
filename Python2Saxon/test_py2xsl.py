0ebbdefb1b6233a86527272464909e55f4165ca9
briesenberg07 <ries07@uw.edu>
2023-03-09 16:58:42 -0800

    update Python2Saxon

--- Python2Saxon/test_py2xsl.py
+++ Python2Saxon/test_py2xsl.py
@@ -1,4 +1,8 @@
 import os
+
+directory = input("Provide the name of the home directory containing the Saxon HE .jar file\n> ")
+version = input("Prove the version number of the Saxon HE .jar file\n> ")
+
 # filepaths in the os.system command below are evaluated from *directory where the terminal is open*
 # filepaths in the .xsl file are evaluated from that file's location
-os.system(f"java -cp ~/ries/saxon/saxon-he-10.3.jar net.sf.saxon.Transform -s:xsl/test_py2xsl.xsl -xsl:xsl/test_py2xsl.xsl word=theBird")
+os.system(f"java -cp ~/{directory}/saxon-he-{version}.jar net.sf.saxon.Transform -s:test_py2xsl.xsl -xsl:test_py2xsl.xsl word=theBird")
